28712ff146e96bb8ba149fef142d2ee978441a31
renansouzzzz <renansouza.oliv@outlook.com>
2022-07-21 18:49:16 -0300

    js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,15 @@
-// funcao declara e-mail c/ alert
+window.addEventListener('DOMContentLoaded', () => { 
+    VANTA.NET({
+      el: ".jumbotron",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x6969b6,
+      backgroundColor: 0x161616
+    })
 
-function alert(){
-    alert('E-mail: renansouza.oliv@outlook.com');
-}
-
-alert();
+})
